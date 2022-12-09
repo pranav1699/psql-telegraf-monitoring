b673a40019c103a53bbae5f23cb7c638d35a2daf
pranav1699 <pranavsadagopan@gmail.com>
2022-12-09 17:21:28 +0530

    graph and plotly package added

--- app.py
+++ app.py
@@ -2,11 +2,19 @@ import streamlit as st
 from sqlalchemy import create_engine
 import pandas as pd
 import os
+from streamlit.components.v1 import html
+import plotly.express as px
+st.set_page_config(layout="wide")
 
 connection_string = user = os.getenv('OUTPUT_DATABASE')
 db = create_engine(connection_string)
 conn = db.connect()
 
+smtp = "select count(distinct(db)) from postgresql"
+cun = conn.execute(smtp) 
+postgres = pd.DataFrame(cun.fetchall(), columns=cun.keys())
+ct=postgres['count']
+st.metric("Total databases",ct)
 
 smtp = "select * from postgresql"
 
@@ -16,3 +24,11 @@ postgres = pd.DataFrame(result.fetchall(), columns=result.keys())
 
 st.dataframe(postgres)
 
+smtp = '''select time,db,avg(tup_fetched) as tup_fetched from postgresql
+group by "time","db" order by time desc
+'''
+res = conn.execute(smtp) 
+
+postgres_ = pd.DataFrame(res.fetchall(), columns=res.keys())
+fig = px.line(postgres_,x='time',y='tup_fetched',color='db')
+st.plotly_chart(fig)
